48cc7543dc3db140f2287fcc4388bb93b41025ff
espmancer <aidenpike113@gmail.com>
2025-10-09 15:46:03 -0400

    Define App class

--- App.py
+++ App.py
@@ -1,9 +1,14 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.title("GLG-PY")
-root.configure(background="blue")
-root.minsize(200, 200)
-root.maxsize(500, 500)
-root.geometry("300x300+50+50")
-root.mainloop()
+class App:
+    def __init__(root):
+        root.title("GLG")
+        root.minsize(200, 200)
+        root.maxsize(500, 500)
+        root.geometry("300x300+50+50")
+    def launch(root):
+        root.mainloop()
+
+App.launch(tk.Tk())
+
+

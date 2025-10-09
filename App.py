38e3e04d9b5e99e1b3b58a451c8d78c1969e7af7
espmancer <aidenpike113@gmail.com>
2025-10-09 15:36:35 -0400

    Rename gui to App

---
+++ App.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("GLG-PY")
+root.configure(background="blue")
+root.minsize(200, 200)
+root.maxsize(500, 500)
+root.geometry("300x300+50+50")
+root.mainloop()

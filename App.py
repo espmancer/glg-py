8bd2dc0f5e391014046e4c24f34ea7f44ac7568e
espmancer <aidenpike113@gmail.com>
2025-10-09 16:15:43 -0400

    Write __init__ for App class

--- App.py
+++ App.py
@@ -1,14 +1,18 @@
 import tkinter as tk
 
-class App:
-    def __init__(root):
-        root.title("GLG")
-        root.minsize(200, 200)
-        root.maxsize(500, 500)
-        root.geometry("300x300+50+50")
-    def launch(root):
-        root.mainloop()
+class App(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        
+        add_item_btn = tk.Button(root, text = "Add Item", command = "add_item")
 
-App.launch(tk.Tk())
+# root.title("GLG")
+# root.minsize(200, 200)
+# root.maxsize(500, 500)
+# root.geometry("300x300+50+50")
 
-
+if __name__ == "__main__":
+    root = tk.Tk()
+    App(root).pack(side="top", fill="both", expand=True)
+    root.mainloop()

a2ef2c33430f85caf94c7b722027bf5d9c08b0c7
espmancer <aidenpike113@gmail.com>
2025-10-16 17:44:23 -0400

    Start laying out main window controls

--- App.py
+++ App.py
@@ -1,18 +1,54 @@
 import tkinter as tk
+from Controller import Controller
 
 class App(tk.Frame):
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
-        self.parent = parent
-        
-        add_item_btn = tk.Button(root, text = "Add Item", command = "add_item")
+    def __init__(self, root):
+        self.root = root
+        self.root.title("GLG")
+        self.root.minsize(200, 200)
+        self.root.maxsize(500, 500)
+        self.root.geometry("300x300+50+50")
+        self.create_widgets()
 
-# root.title("GLG")
-# root.minsize(200, 200)
-# root.maxsize(500, 500)
-# root.geometry("300x300+50+50")
+    def open_item_window(self):
+        self.root.Window()    
+    def create_widgets(self):
+        # Options Grid 
+        options_rows = 3
+        options_columns = 3
+        for columns in range(options_columns):
+            self.root.columnconfigure(columns, weight = 1)
+
+            for rows in range(options_rows):
+                self.root.rowconfigure(rows, weight = 1)
+                            
+        # Items
+        # Item List
+        item_list = Controller.return_item_list()
+        item_listbox = tk.Listbox(root, cnf = item_list)
+        item_listbox.grid(column = 0, row = 0, rowspan = 3)
+        # Add Item
+        add_item_btn = tk.Button(root, text = "Add Item", command = Controller.add_item)
+        add_item_btn.grid(column = 1, row = 0, padx = 3)
+        # Edit Item
+        edit_item_btn = tk.Button(root, text = "Edit Item")
+        edit_item_btn.grid(column = 1, row = 1, padx = 3)
+        # Remove Item
+        remove_item_btn = tk.Button(root, text = "Remove Item")
+        remove_item_btn.grid(column = 1, row = 2, padx = 3)
+        # Recipes
+        # Add Recipe
+        add_recipe_btn = tk.Button(root, text = "Add Recipe")
+        add_recipe_btn.grid(column = 2, row = 0)
+        # Edit Recipe 
+        edit_recipe_btn = tk.Button(root, text = "Edit Recipe")
+        edit_recipe_btn.grid(column = 2, row = 1)
+        # Remove Recipe
+        remove_recipe_btn = tk.Button(root, text = "Remove Recipe")
+        remove_recipe_btn.grid(column = 2, row = 2)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
-    App(root).pack(side="top", fill="both", expand=True)
+    app = App(root)
     root.mainloop()
--- Controller.py
+++ Controller.py
@@ -1,30 +1,32 @@
+# NOTE: This class will only have one instance, so I am making the functions static.
 from os import getenv
-import App
-
-global debug
-debug = True
 
 class Controller:
     def __init__(self, item_list, current_item_index):
         self.current_item_index = 0
         self.locations = ["College", "Grandparents", "Jordan"]
-        if debug:
-            self.item_list = [{"name":"Element Name","type":"item","aisle": {"College":"A1","Grandparents":"A2","Jordan":"A3"}}]
-        else:
-            path = f"{getenv('LOCALAPPDATA')}\\GLG\\grocery_list.txt"
-            self.item_list = open(path, "r").read() 
-    def add_item(self):
-        item_name = input("Enter item name: ")
-        aisles = {location: input(f"Enter aisle for '{location}': ") for location in self.locations}
-        self.item_list.append({
-                            "name":item_name,
-                            "type":"item",
-                            "aisle":aisles
-                        })
-        self.current_item_index = 0
-    def list_items(self):
-        for i in range(len(self.item_list)):
-            print(f"{i} - {self.item_list[i]['name']}")
+        self.item_list = [{"name":"Element Name","type":"item","aisle": {"College":"A1","Grandparents":"A2","Jordan":"A3"}}]
+        # path = f"{getenv('LOCALAPPDATA')}\\GLG\\grocery_list.txt"
+        # self.item_list = open(path, "r").read() 
+    @staticmethod
+    def add_item():
+        print("Item added!")
+        # item_name = input("Enter item name: ")
+        # aisles = {location: input(f"Enter aisle for '{location}': ") for location in self.locations}
+        # self.item_list.append({
+        #                     "name":item_name,
+        #                     "type":"item",
+        #                     "aisle":aisles
+        #                 })
+        # self.current_item_index = 0
+    @staticmethod
+    def return_item_list():
+        item_list = [{"name":"Element Name","type":"item","aisle": {"College":"A1","Grandparents":"A2","Jordan":"A3"}}]
+        item_names = {}
+        for i in range(len(item_list)):
+            item_names.update({i: item_list[i]['name']})
+
+        return item_names
     def delete_item(self):
         self.current_item_index = int(input("Choose 1 number from the list of items: "))
         print(f"Deleted {self.item_list[self.current_item_index]['name']}.")
@@ -45,3 +47,4 @@ class Controller:
             choice = int(input("Choose 1 number from the list of options: "))
             print(f"Current Aisle of {self.locations[choice]}: {self.item_list[self.current_item_index]['aisle'][self.locations[choice]]}")
             self.item_list[self.current_item_index]['aisle'][self.locations[choice]] = input("New Aisle: ")
+            

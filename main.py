857e62f91774b9e24bb31916ba6d3a72ad6bd95a
espmancer <aidenpike113@gmail.com>
2025-10-24 23:11:53 -0400

    Set up list_frame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # NOTE: Temporary one-file version of the code until I can figure out how to refactor it. 
 import tkinter as tk
+from tkinter import ttk
 
 class Backend:
     def __init__(self, filename="grocery_list.txt"):
@@ -18,7 +19,7 @@ class Backend:
         self.locations = ["College", "Grandparents", "Jordan"]
            
     def close(self, root):
-        print(f"Closing!\nItems:{self.items}\nRecipes:{self.recipes}")
+        print(f"Closing!\nItems:{','.join(self.item_names)}\nRecipes:{self.recipes}")
         with open(self.filename, "w", encoding="utf-8") as f:
             f.write("\n".join(self.items + self.recipes))
 
@@ -36,17 +37,42 @@ class Backend:
 
     def edit_item(self, old_name, entry):
         self.items[self.item_names.index(old_name)] = entry
+        self.item_names = [entry[2:].split('|')[0] for entry in self.items if entry[0] == 'I']
 
 class Frontend:
-    def __init__(self, backend, root = tk.Tk()):
+    def __init__(self, backend, root=tk.Tk()):
         self.backend = backend
         self.root = root
-        root.title("GLG")
+
         root.title("GLG")
         root.minsize(200, 200)
         root.maxsize(500, 500)
         root.geometry("300x300+50+50")
         root.protocol("WM_DELETE_WINDOW", lambda: self.backend.close(root))
+
+        # Main Menu (Notebook)
+        tab_ntbk = ttk.Notebook(root)
+        tab_ntbk.pack(fill="both", expand=True)
+
+        list_frame = tk.Frame(tab_ntbk)
+        item_frame = tk.Frame(tab_ntbk)
+        recipe_frame = tk.Frame(tab_ntbk)
+
+        tab_ntbk.add(list_frame, text="List")
+        tab_ntbk.add(item_frame, text="Items")
+        tab_ntbk.add(recipe_frame, text="Recipes")
+
+        # List Frame layout
+        list_frame.columnconfigure(0, weight=3)
+        list_frame.columnconfigure(1, weight=1)
+        list_frame.rowconfigure(0, weight=1)
+
+        user_list = tk.Text(list_frame, width=25)
+        user_list.grid(column=0, row=0, sticky="nsew", padx=5, pady=5)
+
+        generate_list_btn = tk.Button(list_frame, text="Generate List")
+        generate_list_btn.grid(column=1, row=0, sticky="nsew", padx=5, pady=5)
+
         root.mainloop()
 
 # Main Loop
--- test.py
+++ test.py
@@ -1,5 +1,2 @@
-raw_list = "I:Item A|A1|A2|A3\nI:Item B|A2|A3|A4".split('\n')
-items = [entry for entry in raw_list if entry.startswith("I:")]
-item_names = [entry[2:].split('|')[0] for entry in items if entry[0] == 'I']
-
-print(item_names)
+list = ['a1', 'b1', 'a2']
+print(sorted(list))

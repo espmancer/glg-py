b62e249a0119bdadc867dc25ff64da44618dcad5
espmancer <aidenpike113@gmail.com>
2025-10-28 01:11:11 -0400

    testing

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-list = ['a1', 'b1', 'a2']
-print(sorted(list))
+import tkinter as tk
+root = tk.Tk()
+
+items = ['I:Item A|A2|A3|A4']
+item_lbox = tk.Listbox(root, listvariable=items)
+item_lbox.pack()
+
+tk.mainloop()

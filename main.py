f38f874e9d4256dad7bfef22ca18bc12fb91ecd1
espmancer <aidenpike113@gmail.com>
2025-10-28 01:04:29 -0400

    Configure add item btn

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ class Backend:
         self.items[self.item_names.index(old_name)] = entry
 
     def delete_item(self, name):
-        self.items.remove(self.item_names.index(name))
+        self.items.pop(self.get_item_lists("name").index(name))
 
     def get_recipe_lists(self, section=None):
         recipe_names = [entry[2:].split('|')[0] for entry in self.recipes]
@@ -93,11 +93,13 @@ class Frontend:
         tab_ntbk = ttk.Notebook(root)
         tab_ntbk.pack(fill="both", expand=True)
         tab_ntbk.bind('<<NotebookTabChanged>>', lambda event: self.update_lists())
-
+        
+        # Frames
         list_frame = tk.Frame(tab_ntbk)
         item_frame = tk.Frame(tab_ntbk)
         recipe_frame = tk.Frame(tab_ntbk)
 
+            # Add Frames
         tab_ntbk.add(list_frame, text="List")
         tab_ntbk.add(item_frame, text="Items")
         tab_ntbk.add(recipe_frame, text="Recipes")
@@ -107,9 +109,11 @@ class Frontend:
         list_frame.columnconfigure(1, weight=1)
         list_frame.rowconfigure(0, weight=1)
 
+            # User List (Text Widget)
         user_list = tk.Text(list_frame, width=25)
         user_list.grid(column=0, row=0, sticky="nsew", padx=5, pady=5)
-        
+
+            # Generate List Button
         generate_list_btn = tk.Button(list_frame, text="Generate List", command=lambda: self.backend.generate_list(user_list.get("1.0", "end-1c").split('\n\n')))
         generate_list_btn.grid(column=1, row=0, sticky="nsew", padx=5, pady=5)
 
@@ -119,30 +123,44 @@ class Frontend:
             for row in range(5):
                 item_frame.rowconfigure(row, weight=1)
         
+                # Item Listbox
         self.item_lbox = tk.Listbox(item_frame, listvariable=self.backend.get_item_lists())
         self.item_lbox.grid(column=0, row=0, rowspan=2)
-        add_item_btn = tk.Button(item_frame, text="Add Item", command=lambda: backend.add_item(""))
-        add_item_btn.grid(column=1, row=0)
-        delete_item_btn = tk.Button(item_frame, text="Delete Item", command=lambda: self.backend.delete_item())
-        delete_item_btn.grid(column=1, row=1)
+        self.item_lbox.bind('<<ListboxSelect>>', self.select_item)
+        
+                # Item Name
         item_name_lbl = tk.Label(item_frame, text="Item Name:")
         item_name_lbl.grid(column=0, row=2)
         self.item_name_etr = tk.Entry(item_frame)
         self.item_name_etr.grid(column=1, row=2)
+        
+                # College Aisle
         college_aisle_lbl = tk.Label(item_frame, text="College Aisle:")
         college_aisle_lbl.grid(column=0, row=3)
         self.college_aisle_etr = tk.Entry(item_frame)
         self.college_aisle_etr.grid(column=1, row=3)
+        
+                # Grandparents Aisle
         grandparents_aisle_lbl = tk.Label(item_frame, text="Grandparents Aisle:")
         grandparents_aisle_lbl.grid(column=0, row=4)    
         self.grandparents_aisle_etr = tk.Entry(item_frame)
         self.grandparents_aisle_etr.grid(column=1, row=4)
+        
+                # Jordan Aisle
         jordan_aisle_lbl = tk.Label(item_frame, text="Jordan Aisle:")
         jordan_aisle_lbl.grid(column=0, row=5)
         self.jordan_aisle_etr = tk.Entry(item_frame)
         self.jordan_aisle_etr.grid(column=1, row=5)
-
-        self.item_lbox.bind('<<ListboxSelect>>', self.select_item)
+        
+                # Add Item Button
+        add_item_btn = tk.Button(item_frame, text="Add Item", command=lambda: (self.backend.add_item(
+            f"I:{self.item_name_etr.get()}|{self.college_aisle_etr.get()}|{self.grandparents_aisle_etr.get()}|{self.jordan_aisle_etr.get()}"),
+            self.update_lists()))
+        add_item_btn.grid(column=1, row=0)
+        
+                # Delete Item Button
+        delete_item_btn = tk.Button(item_frame, text="Delete Item", command=lambda: (self.backend.delete_item(self.item_name_etr.get()), self.update_lists()))
+        delete_item_btn.grid(column=1, row=1)
 
             # Recipe Frame
         for col in range(2):
@@ -152,9 +170,9 @@ class Frontend:
 
         self.recipe_lbox = tk.Listbox(recipe_frame, listvariable=self.backend.get_recipe_lists())
         self.recipe_lbox.grid(column=0, row=0, rowspan=2)
-        add_recipe_btn = tk.Button(recipe_frame, text="Add Recipe", command=lambda: backend.add_recipe(""))
+        add_recipe_btn = tk.Button(recipe_frame, text="Add Recipe", command=lambda: (backend.add_recipe(""), self.update_lists))
         add_recipe_btn.grid(column=1, row=0)
-        delete_recipe_btn = tk.Button(recipe_frame, text="Delete Recipe", command=lambda: backend.delete_recipe(""))
+        delete_recipe_btn = tk.Button(recipe_frame, text="Delete Recipe", command=lambda: (backend.delete_recipe(""), self.update_lists))
         delete_recipe_btn.grid(column=1, row=1)
 
         root.mainloop()

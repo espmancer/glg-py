6afd2eeb087cc4918d2d24d04f6870343e081f3c
espmancer <aidenpike113@gmail.com>
2025-11-26 11:05:11 -0500

    ignored

--- tests/test.py
+++ tests/test.py
@@ -1,29 +1,28 @@
-# Configurables Abstract Class: 
-## Contains all functions for manipulation and addition of configurable entities like items.
-# Configurables::Items Class
-## String Name, Dictionary of n locations that map to 1 aisle each
-# Configurables::Recipes Class
-## String Name, Array of n item names
+aisles = ['2A', '3AP', '1B']
+letters = []
+numbers = []
 
-# Backend Class
-## File reading, writing
+# CASES
+# Index is number but next index isn't a number
+# Index is letter but next index isn't a letter
+# Index is number AND next index is a number
+# Index is a letter AND next index is a letter 
+for aisle in aisles:
+    for i in range(len(aisle)):
+        if i+1 == len(aisle):
+            break
 
-class Backend:
-    def __init__(self):
-        pass
-    
-    def save(self, configurable):
-        # Write to file
-        pass
+        if aisle[i].isdigit():
+            if aisle[i+1].isdigit():
+                numbers.append((int) (aisle[i] + aisle[i+1]))
+            else:
+                numbers.append((int) (aisle[i]))
+        
+        if aisle[i].isalpha():
+            if aisle[i+1].isalpha():
+                letters.append(aisle[i] + aisle[i+1])
+            else:
+                letters.append(aisle[i])
 
-    def remove
-
-class Configurables:
-    def __init__(self):
-        self.configurables = []
-        self.save() = Backend.save()
-    
-    def add(self):
-        self.save(configurable)
-
-    def remove(self):
+print(numbers)
+print(letters)

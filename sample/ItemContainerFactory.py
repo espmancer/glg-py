7e22fa9dee74f21c6cf98509645481917ae3243e
espmancer <aidenpike113@gmail.com>
2025-12-10 18:05:00 -0500

    Create ItemContainerFactory

---
+++ sample/ItemContainerFactory.py
@@ -0,0 +1,9 @@
+"""
+ItemContainerFactory
+This is a dataclass that is designed to follow the Simple Factory design pattern.
+ItemContainers (or Recipes) contain a name and an n-sized dictionary of items.
+"""
+@dataclass
+class ItemContainerFactory:
+    name: str
+    items: dict

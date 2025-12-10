8dceedc2271e095a166cd787da4e58a849b07951
espmancer <aidenpike113@gmail.com>
2025-12-10 18:11:15 -0500

    Add addEntity()

--- sample/EntityHandler.py
+++ sample/EntityHandler.py
@@ -6,3 +6,8 @@ an n-sized dictionary of Items and ItemContainers.
 class EntityHandler():
     def __init__(self, entities={}):
         self.entities = entities
+
+    # Add one object to the entities dictionary.
+    def addEntity(self, entity):
+        self.entities.add(entity)
+    

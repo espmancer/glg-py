c07559b1b3125d77cb5d4e8ac73918899b8060f6
espmancer <aidenpike113@gmail.com>
2025-12-10 18:27:47 -0500

    import dataclass

--- sample/ItemContainerFactory.py
+++ sample/ItemContainerFactory.py
@@ -3,6 +3,8 @@ ItemContainerFactory
 This is a dataclass that is designed to follow the Simple Factory design pattern.
 ItemContainers (or Recipes) contain a name and an n-sized dictionary of items.
 """
+from dataclasses import dataclass
+
 @dataclass
 class ItemContainerFactory:
     name: str
--- sample/ItemFactory.py
+++ sample/ItemFactory.py
@@ -3,6 +3,8 @@ ItemFactory
 This is a dataclass that is designed to follow the Simple Factory design pattern. 
 Items contain a name and an n-sized dictionary of aisles based on n number of locations.
 """
+from dataclasses import dataclass
+
 @dataclass
 class ItemFactory():
     name: str 

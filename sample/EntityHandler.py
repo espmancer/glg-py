4d672cc97d535f6e52660731fecaacd36d8184e9
espmancer <aidenpike113@gmail.com>
2025-12-10 18:12:20 -0500

    Add removeEntity()

--- sample/EntityHandler.py
+++ sample/EntityHandler.py
@@ -11,3 +11,7 @@ class EntityHandler():
     def addEntity(self, entity):
         self.entities.add(entity)
     
+    # Remove one object at an index from the entities dictionary.
+    def removeEntity(self, index):
+        self.entities.pop(index)
+    

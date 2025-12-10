112abb6ee9c8b7e91fb53ddce5cfb5190d238b42
espmancer <aidenpike113@gmail.com>
2025-12-10 18:09:31 -0500

    Create EntityHandler

---
+++ sample/EntityHandler.py
@@ -0,0 +1,8 @@
+"""
+EntityHandler
+This class is designed to add, remove, and manipulate
+an n-sized dictionary of Items and ItemContainers.
+"""
+class EntityHandler():
+    def __init__(self, entities={}):
+        self.entities = entities

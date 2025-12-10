e02f7861d733e13b0aed4074617c24357bf98226
espmancer <aidenpike113@gmail.com>
2025-12-10 18:31:54 -0500

    Add Location

---
+++ sample/Location.py
@@ -0,0 +1,8 @@
+"""
+Location
+This class is designed to add, remove, and manipulate
+an n-sized list of locations.
+"""
+class Location():
+    def __init__(self, locations=[]):
+        self.locations = locations

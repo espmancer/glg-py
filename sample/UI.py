15e837633428fd43195506af25323836d1837db9
espmancer <aidenpike113@gmail.com>
2025-12-11 14:51:58 -0500

    fix imports and add mainloop()

--- sample/UI.py
+++ sample/UI.py
@@ -2,18 +2,23 @@
 UI
 This class is designed as the primary window for the frontend.
 """
-from tkinter import tk, ttk
+import tkinter as tk
+from tkinter import ttk
 
 class UI():
-    WIDTH = 650
-    HEIGHT = 350
-
-    def __init__(self, root=tk.Tk()):
+    def __init__(self, entity, root=tk.Tk()):
         self.root = root
-
+        self.entity = entity
+        WIDTH = 650
+        HEIGHT = 350
+        
         root.title("GLG")
         root.geometry(f"{WIDTH}x{HEIGHT}")
         root.protocol("WM_DELETE_WINDOW", lambda: self.close())
         tabNotebook = ttk.Notebook(self.root)
         tabNotebook.pack(fill="both", expand=True)
         tabNotebook.bind('<<NotebookTabChanged>>', lambda event: self.updateLists())
+
+        ListFrame()
+
+        self.root.mainloop()

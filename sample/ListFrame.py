0035d790f07f72df597d7e7013a1262d7bcd5228
espmancer <aidenpike113@gmail.com>
2025-12-11 16:51:02 -0500

    add getListFrame()

--- sample/ListFrame.py
+++ sample/ListFrame.py
@@ -2,27 +2,33 @@
 ListFrame
 This class is designed as the list generation screen.
 """
-from tkinter import Text, Button
+from tkinter import Frame, Text, Button
+
 class ListFrame():
     def __init__(self, frame):
-        self.listFrame = tk.Frame(frame)
+        self.listFrame = Frame(frame)
         self.choice = ""
 
-        listFrame.columnconfigure(0, weight=1)
-        listFrame.columnconfigure(1, weight=1)
-        listFrame.rowconfigure(0, weight=1)
+        self.listFrame.columnconfigure(0, weight=1)
+        self.listFrame.columnconfigure(1, weight=1)
+        self.listFrame.rowconfigure(0, weight=1)
 
         # User List (Text Widget)
-        userList = tk.Text(listFrame, width=25)
+        userList = Text(self.listFrame, width=25)
         userList.grid(column=0, row=0, sticky="nsew", padx=5, pady=5)
 
         # Generate List Button
-        generateListButton = tk.Button(listFrame, text="Generate List", command=lambda: setChoice("generateList"))
+        generateListButton = Button(self.listFrame, text="Generate List", command=lambda: setChoice("generateList"))
         generateListButton.grid(column=1, row=0, sticky="nsew", padx=5, pady=5)
     
-    # Get the button choice
+    # Get the choice of button.
     def getChoice(self) -> str:
         return self.choice
     
+    # Get the listFrame.
+    def getListFrame(self) -> Frame:
+        return self.listFrame 
+
+    # Set the choice of button.
     def setChoice(self, choice):
         self.choice = choice

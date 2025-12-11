12d904a1927b8da2b5f67b28abc7f99e29b691e7
espmancer <aidenpike113@gmail.com>
2025-12-11 16:58:05 -0500

    Add ItemFrame

---
+++ sample/ItemFrame.py
@@ -0,0 +1,34 @@
+"""
+ItemFrame
+This class is designed as the Item configuration screen.
+"""
+from tkinter import Frame, Text, Button
+
+class ItemFrame(Frame):
+    def __init__(self, frame):
+        self.listFrame = Frame(frame)
+        self.choice = ""
+
+        self.listFrame.columnconfigure(0, weight=1)
+        self.listFrame.columnconfigure(1, weight=1)
+        self.listFrame.rowconfigure(0, weight=1)
+
+        # User List (Text Widget)
+        userList = Text(self.listFrame, width=25)
+        userList.grid(column=0, row=0, sticky="nsew", padx=5, pady=5)
+
+        # Generate List Button
+        generateListButton = Button(self.listFrame, text="Generate List", command=lambda: setChoice("generateList"))
+        generateListButton.grid(column=1, row=0, sticky="nsew", padx=5, pady=5)
+    
+    # Get the choice of button.
+    def getChoice(self) -> str:
+        return self.choice
+    
+    # Get the listFrame.
+    def getFrame(self) -> Frame:
+        return self.listFrame 
+
+    # Set the choice of button.
+    def setChoice(self, choice):
+        self.choice = choice

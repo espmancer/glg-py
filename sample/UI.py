a857d5a268c15d7eab92657ea4ba736d4ac00823
espmancer <aidenpike113@gmail.com>
2025-12-11 17:27:48 -0500

    add close()

--- sample/UI.py
+++ sample/UI.py
@@ -5,34 +5,39 @@ This class is designed as the primary window for the frontend.
 import tkinter as tk
 from tkinter import ttk
 from ListFrame import ListFrame
+from ItemFrame import ItemFrame
 
 class UI():
     def __init__(self, entity):
         # UI Variables
-        self.entity = entity
-        root = tk.Tk()
+        self.root = tk.Tk()
         WIDTH = 650
         HEIGHT = 350
         
         # Window Settings
-        root.title("GLG")
-        root.geometry(f"{WIDTH}x{HEIGHT}")
-        root.protocol("WM_DELETE_WINDOW", lambda: self.close())
-        tabNotebook = ttk.Notebook(root)
+        self.root.title("GLG")
+        self.root.geometry(f"{WIDTH}x{HEIGHT}")
+        self.root.protocol("WM_DELETE_WINDOW", lambda: self.close())
+        tabNotebook = ttk.Notebook(self.root)
         tabNotebook.pack(fill="both", expand=True)
         tabNotebook.bind('<<NotebookTabChanged>>', lambda event: self.updateLists())
 
         # Frame Variables
-        listFrame = ListFrame(root).getFrame()
-        # itemFrame = ItemFrame(root).getListFrame()
-        # itemContainerFrame = ItemContainerFrame(root).getListFrame()
-        # locationFrame = locationFrame(root).getListFrame()
+        listFrame = ListFrame(self.root).getFrame()
+        itemNames = [entity for entity in entity.getEntities()]
+        print(itemNames)
+        itemFrame = ItemFrame(self.root).getFrame()
+        # itemContainerFrame = ItemContainerFrame(self.root).getFrame()
+        # locationFrame = locationFrame(self.root).getFrame()
         
         # Add all frames
         tabNotebook.add(listFrame, text="List")
-        # tabNotebook.add(itemFrame, text="Items")
+        tabNotebook.add(itemFrame, text="Items")
         # tabNotebook.add(itemContainerFrame, text="Recipes")
         # tabNotebook.add(locationFrame, text="Locations")
         
         # Main Loop
-        root.mainloop()
+        self.root.mainloop()
+
+    def close(self):
+        self.root.destroy()

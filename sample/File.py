2f548d44f69ccffc6e23c1e2efac1be0b42e2e34
espmancer <aidenpike113@gmail.com>
2025-12-11 00:06:05 -0500

    Add readFile()

--- sample/File.py
+++ sample/File.py
@@ -5,11 +5,22 @@ This class is designed to read and write to the grocery_list.txt file.
 class File():
     def __init__(self, path="grocery_list.txt"):
         self.path = path
+        self.f = None
+
+        self.openOrCreateFile()
 
     # See if there is a file at the provided path, create one if there isn't.
-    def openOrCreateFile():
+    def openOrCreateFile(self):
         try:
-            open(path, "r")
+            self.f = open(self.path, "rw")
         except FileNotFoundError:
-            with open(path, "a+") as f:
-                f.write()
+            with open(self.path, "a+") as self.f:
+            self.f.write("")
+
+    # Get all contents of the file at the provided path.
+    def readFile(self) -> str:
+        return self.f.read()
+
+    def writeFile(self, string):
+        self.f.write(string)
+

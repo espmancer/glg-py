97a921b09c42ce8baaf725e3ea105748674b97f7
espmancer <aidenpike113@gmail.com>
2025-12-11 16:51:17 -0500

    add ListFrame to UI

--- sample/UI.py
+++ sample/UI.py
@@ -2,22 +2,31 @@
 UI
 This class is designed as the primary window for the frontend.
 """
+import tkinter as tk
 from tkinter import ttk
+from ListFrame import ListFrame
 
 class UI():
-    def __init__(self, entity, root=tk.Tk()):
-        self.root = root
+    def __init__(self, entity):
+        # UI Variables
         self.entity = entity
+        root = tk.Tk()
         WIDTH = 650
         HEIGHT = 350
         
+        # Window Settings
         root.title("GLG")
         root.geometry(f"{WIDTH}x{HEIGHT}")
         root.protocol("WM_DELETE_WINDOW", lambda: self.close())
-        tabNotebook = ttk.Notebook(self.root)
+        tabNotebook = ttk.Notebook(root)
         tabNotebook.pack(fill="both", expand=True)
         tabNotebook.bind('<<NotebookTabChanged>>', lambda event: self.updateLists())
 
-        self.root.mainloop()
-
+        # Frame Variables
+        listFrame = ListFrame(root).getListFrame()
+        
+        # Add all frames
+        tabNotebook.add(listFrame, text="List")
+        root.mainloop()
+    
     

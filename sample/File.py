5d988f34e1d166dc87c2f906a186979b68fa4045
espmancer <aidenpike113@gmail.com>
2025-12-10 21:39:38 -0500

    Add openOrCreateFile()

--- sample/File.py
+++ sample/File.py
@@ -5,3 +5,11 @@ This class is designed to read and write to the grocery_list.txt file.
 class File():
     def __init__(self, path="grocery_list.txt"):
         self.path = path
+
+    # See if there is a file at the provided path, create one if there isn't.
+    def openOrCreateFile():
+        try:
+            open(path, "r")
+        except FileNotFoundError:
+            with open(path, "a+") as f:
+                f.write()

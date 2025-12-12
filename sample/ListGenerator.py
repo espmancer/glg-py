5f56b91955730e4de44163a895d1becf47fa373a
espmancer <aidenpike113@gmail.com>
2025-12-11 20:32:26 -0500

    Add generateList() and getList()

--- sample/ListGenerator.py
+++ sample/ListGenerator.py
@@ -3,8 +3,25 @@ ListGenerator
 This class is designed as the name entails.
 """
 class ListGenerator():
-    def __init__(self, userList, entity):
+    def __init__(self, userList, entityHandler, location):
         self.userList = userList
-        entityList = entity.getEntities()
+        self.entityList = entityHandler.getEntities()
+        self.currentLocation = location.getCurrentLocation()
         self.finalList = []
 
+    def generateList(self):
+        for entity in self.userList.split():
+            if self.entityList[entity].kind == "Item":
+                aisle = self.entityList[entity].aisle[self.currentLocation]
+                name = self.entityList[entity].name
+
+                self.finalList.add(f"- [ ] ({aisle}) {name}")
+            else:
+                for item in self.entityList[entity].items:
+                    aisle = self.entityList[item].items[self.currentLocation]
+                    name = self.entityList[item].name
+
+                    self.finalList.add(f"- [ ] ({aisle}) {name}")
+    
+    def getList(self) -> list:
+        return self.finalList

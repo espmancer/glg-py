50061c4a34ca704a4c657e9dda5b4ccbb05e082d
espmancer <aidenpike113@gmail.com>
2025-12-12 23:35:09 -0500

    rename itemFrame variables

--- sample/ItemFrame.py
+++ sample/ItemFrame.py
@@ -2,32 +2,33 @@
 ItemFrame
 This class is designed as the Item configuration screen.
 """
-from tkinter import Frame, Text, Button
+from tkinter import Frame, Listbox, Label, Entry, OptionMenu
 
 class ItemFrame(Frame):
-    def __init__(self, frame):
-        self.listFrame = Frame(frame)
+    def __init__(self, frame, itemNames=[]):
+        self.itemFrame = Frame(frame)
         self.choice = ""
+        columnCount = 3
+        rowCount = 7
 
-        self.listFrame.columnconfigure(0, weight=1)
-        self.listFrame.columnconfigure(1, weight=1)
-        self.listFrame.rowconfigure(0, weight=1)
+        for column in range(columnCount):
+            self.itemFrame.columnconfigure(column, weight=1)
+            
+            for row in range(rowCount):
+                self.itemFrame.rowconfigure(row, weight=1)
 
-        # User List (Text Widget)
-        userList = Text(self.listFrame, width=25)
-        userList.grid(column=0, row=0, sticky="nsew", padx=5, pady=5)
+        # Item Listbox
+        self.itemListbox = Listbox(self.itemFrame, listvariable=itemNames)
+        self.itemListbox.bind('<<ListboxSelect>>', lambda: setChoice("selectItem"))
+        self.itemListbox.grid(column=0, row=0, rowspan=4)
 
-        # Generate List Button
-        generateListButton = Button(self.listFrame, text="Generate List", command=lambda: setChoice("generateList"))
-        generateListButton.grid(column=1, row=0, sticky="nsew", padx=5, pady=5)
-    
     # Get the choice of button.
     def getChoice(self) -> str:
         return self.choice
     
     # Get the listFrame.
     def getFrame(self) -> Frame:
-        return self.listFrame 
+        return self.itemFrame 
 
     # Set the choice of button.
     def setChoice(self, choice):

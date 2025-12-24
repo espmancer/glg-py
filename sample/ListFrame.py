86610aeed871f704e27629cd9da8c7bd4e49246b
espmancer <aidenpike113@gmail.com>
2025-12-24 15:39:16 -0500

    fix parents for grid

--- sample/ListFrame.py
+++ sample/ListFrame.py
@@ -2,17 +2,21 @@
 ListFrame
 This class is designed as the list generation screen.
 """
-from tkinter as tk
+import tkinter as tk
 
-class ListFrame(Frame):
+class ListFrame(tk.Frame):
     def __init__(self, root):
         super().__init__()
 
+        # Set Up Grid
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.rowconfigure(0, weight=1)
         # User List (Text Widget)
-        self.userList = Text(root, width=25)
+        self.userList = tk.Text(self, width=25)
         self.userList.grid(column=0, row=0, sticky="nsew", padx=5, pady=5)
         # Generate List Button
-        self.generateListButton = Button(root, text="Generate List", command=lambda: self.event_generate("<<generateList>>"))
+        self.generateListButton = tk.Button(self, text="Generate List", command=lambda: self.event_generate("<<generateList>>"))
         self.generateListButton.grid(column=1, row=0, sticky="nsew", padx=5, pady=5)
 
     # Get the user list.

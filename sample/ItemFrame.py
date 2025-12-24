dde02963990cb99533ce3e46fc3a111b313dd0f0
espmancer <aidenpike113@gmail.com>
2025-12-24 15:38:39 -0500

    fix virtual event generation for ItemFrame and ListFrame

--- sample/ItemFrame.py
+++ sample/ItemFrame.py
@@ -2,29 +2,31 @@
 ItemFrame
 This class is designed as the Item configuration screen.
 """
-from tkinter import Frame, Listbox, Label, Entry, OptionMenu, Button
+import tkinter as tk
 
-class ItemFrame(Frame):
+class ItemFrame(tk.Frame):
     def __init__(self, frame, itemNames=[]):
-        self.itemFrame = Frame(frame)
+        super().__init__()
+
         self.choice = ""
         columnCount = 3
         rowCount = 7
 
         for column in range(columnCount):
-            self.itemFrame.columnconfigure(column, weight=1)
+            self.columnconfigure(column, weight=1)
             
             for row in range(rowCount):
-                self.itemFrame.rowconfigure(row, weight=1)
+                self.rowconfigure(row, weight=1)
 
         # Item Listbox
-        self.itemListbox = Listbox(self.itemFrame, listvariable=itemNames)
-        self.itemListbox.bind('<<ListboxSelect>>', lambda: self.itemFrame.generate_event("<<getEntity>>"))
+        self.itemListbox = tk.Listbox(self, listvariable=itemNames)
         self.itemListbox.grid(column=0, row=0, rowspan=4)
-
         # Add Item Button
-        self.addItemButton = Button(self.itemFrame)
-    
-    # Get the listFrame.
-    def getFrame(self) -> Frame:
-        return self.itemFrame 
+        self.addItemButton = tk.Button(self, text="Save New Item", command=lambda: self.event_generate("<<addItem>>"))
+        self.addItemButton.grid(column=0, row=4, columnspan=3)
+        # Edit Item Button
+        self.editItemButton = tk.Button(self, text="Save Item", command=lambda: self.event_generate("<<editItem>>"))
+        self.editItemButton.grid(column=0, row=5, columnspan=3)
+        # Remove Item Button
+        self.removeItemButton = tk.Button(self, text="Remove Item", command=lambda: self.event_generate("<<removeItem>>"))
+        self.removeItemButton.grid(column=0, row=6, columnspan=3)
--- sample/ListFrame.py
+++ sample/ListFrame.py
@@ -2,25 +2,19 @@
 ListFrame
 This class is designed as the list generation screen.
 """
-from tkinter import Frame, Text, Button
+from tkinter as tk
 
 class ListFrame(Frame):
-    def __init__(self, frame):
-        self.listFrame = Frame(frame)
-        self.choice = ""
-
-        self.listFrame.columnconfigure(0, weight=1)
-        self.listFrame.columnconfigure(1, weight=1)
-        self.listFrame.rowconfigure(0, weight=1)
+    def __init__(self, root):
+        super().__init__()
 
         # User List (Text Widget)
-        self.userList = Text(self.listFrame, width=25)
+        self.userList = Text(root, width=25)
         self.userList.grid(column=0, row=0, sticky="nsew", padx=5, pady=5)
-
         # Generate List Button
-        self.generateListButton = Button(self.listFrame, text="Generate List", command=lambda: self.listFrame.event_generate("<<generateList>>"))
+        self.generateListButton = Button(root, text="Generate List", command=lambda: self.event_generate("<<generateList>>"))
         self.generateListButton.grid(column=1, row=0, sticky="nsew", padx=5, pady=5)
-    
-    # Get the listFrame.
-    def getFrame(self) -> Frame:
-        return self.listFrame 
+
+    # Get the user list.
+    def getUserList(self) -> str:
+        return self.userList.get("1.0", "end")

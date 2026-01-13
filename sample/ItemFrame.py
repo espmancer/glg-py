728e40a81f95091ae4d41cdcd79f930ce54f5d0b
espmancer <aidenpike113@gmail.com>
2026-01-12 23:07:44 -0500

    Add item name, aisle, and location dropdown

--- sample/ItemFrame.py
+++ sample/ItemFrame.py
@@ -5,7 +5,7 @@ This class is designed as the Item configuration screen.
 import tkinter as tk
 
 class ItemFrame(tk.Frame):
-    def __init__(self, frame, itemNames=[]):
+    def __init__(self, frame, currentLocation, locations, itemNames=[]):
         super().__init__()
 
         columnCount = 3
@@ -20,6 +20,20 @@ class ItemFrame(tk.Frame):
         # Item Listbox
         self.itemListbox = tk.Listbox(self, listvariable=itemNames)
         self.itemListbox.grid(column=0, row=0, rowspan=4)
+        # Location Option Menu
+        locationOptionMenu = tk.OptionMenu(
+            self,
+            tk.StringVar(value=currentLocation),
+            *locations,
+            command=lambda event: self.event_generate("<<setLocation>>")
+        )
+        locationOptionMenu.grid(column=1,row=1)
+        # Item Name
+        self.itemName = tk.Entry(self)
+        self.itemName.grid(column=2,row=0)
+        # Item Aisle
+        self.itemAisle = tk.Entry(self)
+        self.itemAisle.grid(column=2,row=1)
         # Add Item Button
         self.addItemButton = tk.Button(self, text="Save New Item", command=lambda: self.event_generate("<<addItem>>"))
         self.addItemButton.grid(column=0, row=4, columnspan=3)
@@ -30,8 +44,12 @@ class ItemFrame(tk.Frame):
         self.removeItemButton = tk.Button(self, text="Remove Item", command=lambda: self.event_generate("<<removeItem>>"))
         self.removeItemButton.grid(column=0, row=6, columnspan=3)
 
-    def updateLists(self,entityNames):
+    def updateLists(self, entityNames):
         self.itemListbox.delete(0,tk.END)
         
         for entityName in entityNames:
             self.itemListbox.insert(tk.END, entityName)
+    
+    def setText(self, entry, text):
+        entry.delete(0, tk.END)
+        entry.insert(0, text)
